7e13d441008a0f6b67dfeeb3e82a51d94f767bc7
Administrator <Administrator@DESKTOP-ITO9GJ1>
2025-07-26 09:17:42 +0300

    Add project files.

---
+++ CodeFirstBasic/Context/PatikaFirstDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CodeFirstBasic.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstBasic.Context
+{
+    public class PatikaFirstDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+    "Server=localhost\\SQLEXPRESS;Database=PatikaCodeFirst1;Trusted_Connection=True;TrustServerCertificate=True;");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    
+
+    public DbSet<MovieEntity> Movies => Set<MovieEntity>();
+    public DbSet<GameEntity> Games => Set<GameEntity>();
+
+    }
+}
